26fe783fee02949c485356dfa637e511485ddc91
Belen Dominguez <b.dominguez@cathedralsw.com>
2021-04-04 10:07:58 +0200

    Fixed error with the intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,8 +26,8 @@ function Index() {
     };
 
     return (
-      <div className="bg-stars w-full h-screen relatve">
-        <div className="left-2/4 opacity-0 absolute transform scale-50 top-2/4 z-200 items-center flex justify-center w-72" ref={title}>
+      <div className="bg-stars w-full h-screen relatve justify-center items-center flex">
+        <div className="left-2/4 opacity-0 absolute transform scale-50 top-2/4 items-center flex justify-center w-72" ref={title}>
           <Logo />
         </div>
       </div>
